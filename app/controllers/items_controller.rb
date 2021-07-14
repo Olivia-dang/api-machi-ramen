994fe6d240c1f9e0346c3064bd7dba3a09f7ebc2
Olivia <olivia.phuonganh@gmail.com>
2021-07-14 10:27:49 +1000

    added test for controller in client.http

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,18 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:destroy]
+  before_action :set_item, only: [:show, :update, :destroy]
+
+  # GET /items
   def index
     @items = Item.all 
     render json: @items
   end
+
+  # GET /items/1
+  def show
+    render json: @item
+  end
+
+  # POST /items
   def create
     @item = Item.new(item_params)
 
@@ -13,6 +22,17 @@ class ItemsController < ApplicationController
       render json: @item.errors, status: :unprocessable_entity
     end
   end
+
+  # PATCH/PUT /items/1
+  def update
+    if @item.update(item_params)
+      render json: @item
+    else
+      render json: @item.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /items/1
   def destroy
     @item.destroy
   end
