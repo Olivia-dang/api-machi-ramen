460efd7f04990bc02027ba880de0eb57ea8e7800
Olivia <olivia.phuonganh@gmail.com>
2021-07-22 15:10:32 +1000

    test user registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::API
     before_action :configure_permitted_parameters, if: :devise_controller?
     include Pundit
+    rescue_from ActiveRecord::RecordNotUnique, with: :duplicate_record_response
     
     protected
+
+    def duplicate_record_response(exception)
+        render json: { error: exception.message }, status: :bad_request
+    end
 
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:role, :first_name])
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,10 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+# Requires supporting ruby files 
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -70,4 +74,6 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.include DeviseRequestSpecHelpers, type: :request
+  config.include UserHelpers
+
 end
