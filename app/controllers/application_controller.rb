5608f08ad423fb054c8ca653bd6bd9e9858966b3
Olivia <olivia.phuonganh@gmail.com>
2021-07-22 17:23:31 +1000

    added tests for items and categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,17 @@ class ApplicationController < ActionController::API
     before_action :configure_permitted_parameters, if: :devise_controller?
     include Pundit
     rescue_from ActiveRecord::RecordNotUnique, with: :duplicate_error_response
-  
+    rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
+      
     protected
     # rescue method for duplicate email error
     def duplicate_error_response(exception)
         render json: { error: exception.message }, status: :bad_request
+    end
+
+    # rescue method when couldn't find the record requested
+    def record_not_found(exception)
+        render json: { error: exception.message }, status: :not_found
     end
 
     def configure_permitted_parameters
