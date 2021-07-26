44bcea2d3e57d7650c2ee1cb6848919b98fbb64e
Olivia-dang <78009350+Olivia-dang@users.noreply.github.com>
2021-07-26 11:48:50 +1000

    fix image uploading error (#8)
    * error handling no image for new item
    
    * fixed image uploading test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,6 +25,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  routes.default_url_options[:host] = 'localhost:3000'
   config.cache_store = :null_store
 
   # Raise exceptions instead of rendering exception templates.
